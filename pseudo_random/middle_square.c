306409e303a371fb849e112e3ec6144b5cf20c44
Nikola Vojkic <nvojkic@k0r1p9.42mulhouse.fr>
2022-03-29 18:25:20 +0200

    added middle_square

---
+++ pseudo_random/middle_square.c
@@ -0,0 +1,33 @@
+/* 
+ * Simple implementation of midle square method for
+ * generating pseudorandom number in a range [0..n].
+ *
+ * Simply call myRandInRange(int min, int max)
+ * 		 ! max-min must be < 100000 !
+ *
+ * You can change the initial seed by setting next
+ * to another 4 digit number excluding these :
+ * 0000, 0100, 0504, 5030, 3009, 2500, 3792, 7600,
+ * 2916 but 3251 is a pretty good seed.
+ *
+ * After a certain of repetition the generator get 
+ * stuck and output only zeroes or a repetitive
+ * sequence.
+ * 
+ * https://en.wikipedia.org/wiki/Middle-square_method 
+ */
+
+int	ft_rand(void)
+{
+	static int	next = 3251;
+
+	next = ((next * next) / 100) % 10000;
+	return (next);
+}
+
+int	ft_rand_range(int min, int max)
+{
+	return (ft_rand() % (max + 1 - min) + min);
+}
+
+/* call ft_rand_range with a range [0, n] */
